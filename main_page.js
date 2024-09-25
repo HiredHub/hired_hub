e6c8b7bc3143f814bb977ecac78133cdd552765a
Degorck <degorckterr@gmail.com>
2024-09-25 17:51:15 -0600

    Added new_position page

---
+++ main_page.js
@@ -0,0 +1,3 @@
+document.getElementById('go_to_new_position_page').addEventListener('click', function() {
+  window.location.href = 'new_position_page.html'; // Navigate back to the first HTML file
+});
